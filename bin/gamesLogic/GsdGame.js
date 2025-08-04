bfa9ddbb657e29312eacfcd2e438bbe078ccb907
vlad <vladislav.nikolaenko@p2p.org>
2025-08-04 16:29:30 +0700

    fix: minor logic or doc update

--- bin/gamesLogic/GsdGame.js
+++ bin/gamesLogic/GsdGame.js
@@ -10,7 +10,7 @@ export class GsdGame {
     const num1 = getRandomNumber(1, 100)
     const num2 = getRandomNumber(1, 100)
 
-    const question = `${num1} and ${num2}`
+    const question = `${num1} ${num2}`
     const correctAnswer = getGsd(num1, num2)
 
     return { question, correctAnswer }
